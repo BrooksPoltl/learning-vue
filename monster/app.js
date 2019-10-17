82eac6f0121ebc4457631ef6ee9c133bcff543bf
brookspoltl <bpoltl1@gmail.com>
2019-10-16 20:35:54 -0500

    start game method created

--- monster/app.js
+++ monster/app.js
@@ -5,5 +5,13 @@ new Vue({
         monsterHealth: 100,
         gameIsRunning: false,
 
+    },
+    methods: {
+        startGame(){
+            this.gameIsRunning = true;
+            this.playerHealth = 100;
+            this.monsterHealth = 100;
+
+        }
     }
 })
