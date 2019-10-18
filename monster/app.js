7799e720edc7b016d74e3919b26a63904ed72741
BrooksPoltl <bpoltl1@gmail.com>
2019-10-18 13:55:03 -0500

    more attack refactor

--- monster/app.js
+++ monster/app.js
@@ -15,20 +15,13 @@ new Vue({
         },
         attack(){ 
             this.monsterHealth -= this.calculateDamage(3,10);
-            
-            if(this.monsterHealth <= 0){
-                alert('You won!');
-                this.gameIsRunning = false;
+
+            if(this.checkWin()){
                 return;
             }
-
             this.playerHealth -= this.calculateDamage(5, 12);
 
-            if(this.playerHealth <= 0){
-                alert('You lost!');
-                this.gameIsRunning = false;
-                return;
-            }
+            this.checkWin();
         },
         specialAttack(){
 
@@ -41,6 +34,25 @@ new Vue({
         },
         calculateDamage(min,max){
             return Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin(){
+            if(this.monsterHealth <= 0){
+                if(confirm('You won! New Game?')){
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            else if (this.playerHealth <= 0){
+                if(confirm('You lost! New Game?')){
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
         }
     }
 })
