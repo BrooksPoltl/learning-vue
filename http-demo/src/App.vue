fb1466b08d208f8e88bfd25b8f59addb7967c6dc
BrooksPoltl <bpoltl1@gmail.com>
2019-10-28 20:48:09 -0500

    http post with frebase

--- http-demo/src/App.vue
+++ http-demo/src/App.vue
@@ -3,6 +3,15 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Http</h1>
+                <div class="form-group">
+                    <label>Username</label>
+                    <input type="text" class="form-control" v-model = "user.username">
+                </div>
+                <div class="form-group">
+                    <label>Mail</label>
+                    <input type="text" class="form-control" v-model = "user.email">
+                </div>
+                <button class="btn btn-primary" @click = "submit">Submit</button>
             </div>
         </div>
     </div>
@@ -10,6 +19,24 @@
 
 <script>
     export default {
+        data() {
+            return{
+                user: {
+                    username:'',
+                    email: ''
+                }
+            }
+        },
+        methods: {
+            submit() {
+                this.$http.post('https://http-demo-d4999.firebaseio.com/data.json', this.user)
+                .then( res => {
+                        console.log(res);
+                    }, error =>{
+                        console.log(error);
+                    })
+            }
+        }
     }
 </script>
 
