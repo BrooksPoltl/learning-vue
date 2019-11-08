5387a97e1db9f3f49699932b39d510f853060128
BrooksPoltl <bpoltl1@gmail.com>
2019-11-07 19:54:13 -0600

    implemented buy method

--- stock-trader/src/components/stocks/Stocks.vue
+++ stock-trader/src/components/stocks/Stocks.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <app-stock v-for ="stock in stocks" :key = "stock.id"></app-stock>
+        <app-stock v-for ="stock in stocks" :stock = "stock" :key = "stock.id"></app-stock>
     </div>
 </template>
 
