f34aadee68dacfe160cf78df42c43df5e9da4962
BrooksPoltl <bpoltl1@gmail.com>
2019-11-08 13:25:32 -0600

    added vuex

--- stock-trader/src/main.js
+++ stock-trader/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import VueRouter from 'vue-router';
 
 import { routes } from './routes';
+import store from './store/store';
+
 
 Vue.use(VueRouter);
 
@@ -14,5 +16,6 @@ const router = new VueRouter({
 new Vue({
   el: '#app',
   router,
+  store,
   render: h => h(App)
 })
